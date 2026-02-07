ebcb6468befaab42fcd4d5097d0d96b9b78766db
fashionpizza <azer7stambul1030@gmail.com>
2026-02-07 16:57:18 +0600

    changed words

--- script.js
+++ script.js
@@ -3,12 +3,10 @@ mainTitle.addEventListener('mouseenter', () => {
   console.log(mainTitle.textContent);
 });
 
-const colorChangeBtn = document.getElementById('color-change-btn');
-const colors = ['#f093f0', '#0eff1a'];
+const colorBtn = document.querySelector('#color-btn');
+colorBtn.toggled = false;
 
-colorChangeBtn.toggled = false;
-
-colorChangeBtn.addEventListener('click', () => {
-  colorChangeBtn.style.backgroundColor = colors[+colorChangeBtn.toggled];
-  colorChangeBtn.toggled = !colorChangeBtn.toggled;
+colorBtn.addEventListener('click', () => {
+  colorBtn.style.backgroundColor = colorBtn.toggled ? '#f093f0' : '#0eff1a';
+  colorBtn.toggled = !colorBtn.toggled;
 });
