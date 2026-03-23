e9514b1fec65fbf8d236feaca93549fc05f02a62
fashionpizza <azer7stambul1030@gmail.com>
2026-03-23 22:22:18 +0600

    render product cards with images

---
+++ product-cards.js
@@ -0,0 +1,62 @@
+export const productCards = [
+  {
+    title: "Увлажняющий мусс",
+    skinType: "для нормальной кожи",
+    description: "Глубоко увлажняют кожу лица, оставляя её мягкой и гладкой.",
+    composition: [
+      "активные натуральные комплексы",
+      "витамины С, А, РР, В И Е",
+      "солнцезащитные компоненты"
+    ],
+    price: 2750,
+    img: "/images/moisturizing-mousse.png"
+  },
+  {
+    title: "Увлажняющая маска",
+    skinType: "для нормальной кожи",
+    description: "Способствует удерживанию влаги в верхних слоях кожи.",
+    composition: [
+      "воски",
+      "минералы",
+      "масла"
+    ],
+    price: 3500,
+    img: "/images/moisturizing-mask.png"
+  },
+  {
+    title: "Гель для умывания",
+    skinType: "для нормальной кожи",
+    description: "Интенсивно очищает, не повреждает защитный барьер кожи.",
+    composition: [
+      "минералы",
+      "витамины С, А, РР, В И Е",
+      "солнцезащитные компоненты"
+    ],
+    price: 1650,
+    img: "/images/face-wash-gel.png"
+  },
+  {
+    title: "Подарочный набор №1",
+    skinType: "для нормальной кожи",
+    description: "Набор, состоящий из увлажняющего крема и маски.",
+    composition: [
+      "воски",
+      "минералы",
+      "масла"
+    ],
+    price: 4750,
+    img: "/images/gift-set-number-1.png"
+  },
+  {
+    title: "Подарочный набор №5",
+    skinType: "для нормальной кожи",
+    description: "Весь набор средств Invisible symphony, крем, маска, мусс и гель для умывания.",
+    composition: [
+      "воски",
+      "минералы",
+      "масла"
+    ],
+    price: 7520,
+    img: "/images/gift-set-number-5.png"
+  }
+];
